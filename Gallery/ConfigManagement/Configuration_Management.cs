b3b5e519aed3dc48a6b0173b854e77cb16317742
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-02-12 21:34:09 +0200

    A new class has been created, with the help of which default values of keys are filled in case of their absence

---
+++ Gallery/ConfigManagement/Configuration_Management.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace Gallery.ConfigManagement
+{
+    public class Configuration_Management
+    {
+        //
+        //Variables for storing key data
+        public string pathToPhotos { get; } = WebConfigurationManager.AppSettings["PathToPhotos"];
+        public string fileExtensions { get; } = WebConfigurationManager.AppSettings["FileExtensions"];
+        //
+        //Default constants
+        private const string defaultValuePathToPhotos = "/Content/Images/";
+        private const string defaultValueFileExtensions = "image/jpeg;image/png";
+
+        public static void AddDefaultValueAppSettings(string key, string value)//Adding a default value  
+        {  
+            try  
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    if (settings[key].Value == "")
+                    {
+                        settings[key].Value = value;
+                    }
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }  
+            catch (ConfigurationErrorsException)  
+            {
+                //Add error
+            }
+        } 
+    }
+}
