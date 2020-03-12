66fa1abeebe3c8ef857fa3a826eaac8e2e566d95
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-03-12 21:02:06 +0200

    Добавлено в проект в папку App_Start класс (OWIN) Startup

---
+++ Gallery/Gallery/App_Start/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.RightsManagement;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(Gallery.App_Start.Startup))]
+
+namespace Gallery.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/Account/Login"),
+            });
+
+        }
+    }
+}
