f2b2957cf1c17093d3b83b304bf11973333866c8
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-03-12 23:18:19 +0200

    Login and password validation attributes added

--- Gallery/Gallery.DAL/Models/Models.cs
+++ Gallery/Gallery.DAL/Models/Models.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,26 @@ namespace Gallery.DAL.Models
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "Field must be set")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Line length must be between 3 and 30 characters")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Field must be set")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Line length must be between 3 and 30 characters")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 
     public class RegisterModel
     {
+        [Required(ErrorMessage = "Field must be set")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Line length must be between 3 and 30 characters")]
         public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Field must be set")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Line length must be between 3 and 30 characters")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
