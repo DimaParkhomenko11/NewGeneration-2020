ca5d7fff44a47ed729bc712e0dc549e63ac857a3
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-03-16 02:26:12 +0200

    Cookies cookies via interfaces

---
+++ Gallery/Gallery.BLL/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace Gallery.BLL.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        void OwinCookieAuthentication(IOwinContext owinContext, ClaimsIdentity claim);
+
+        ClaimsIdentity ClaimTypesСreation(string userId);
+    }
+}
---
+++ Gallery/Gallery.BLL/Services/AuthenticationService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gallery.BLL.Interfaces;
+using Microsoft.Owin.Security;
+using System.Security.Claims;
+using Gallery.DAL;
+using Microsoft.Owin;
+
+
+namespace Gallery.BLL.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        public void OwinCookieAuthentication(IOwinContext owinContext, ClaimsIdentity claim)
+        {
+            owinContext.Authentication.SignOut();
+            owinContext.Authentication.SignIn(new AuthenticationProperties
+            {
+                IsPersistent = true
+            }, claim);
+           
+        }
+
+        public ClaimsIdentity ClaimTypesСreation(string userId)
+        {
+            ClaimsIdentity claim = new ClaimsIdentity("ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            claim.AddClaim(new Claim(ClaimTypes.NameIdentifier, userId, ClaimValueTypes.String));
+            claim.AddClaim(new Claim("http://schemas.microsoft.com/accesscontrolservice/2010/07/claims/identityprovider",
+                "OWIN Provider", ClaimValueTypes.String));
+            return claim;
+        }
+    }
+}
