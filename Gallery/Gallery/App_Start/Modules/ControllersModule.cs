b8d6da8946eabe379afff92bc63a0c074980a7aa
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-03-22 21:04:59 +0200

    Added folder and module class for implementing dependency modules

---
+++ Gallery/Gallery/App_Start/Modules/ControllersModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+
+namespace Gallery.App_Start.Modules
+{
+    public class ControllersModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            
+        }
+    }
+}
