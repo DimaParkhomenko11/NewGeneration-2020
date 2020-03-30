e6046829f4725246becafaf8b9be857a7c071c0d
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-03-30 14:32:10 +0300

    Implemented ValidateModelStateAttribute class, added query validation

--- Gallery/Gallery/Filters/ValidateModelStateAttribute.cs
+++ Gallery/Gallery/Filters/ValidateModelStateAttribute.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
+using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
+using System.Windows.Media.Animation;
 
 namespace Gallery.Filters
 {
     public class ValidateModelStateAttribute : ActionFilterAttribute
     {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            var isValid = actionContext.ModelState.IsValid;
 
+            if (!isValid)
+            {
+                actionContext.Response.StatusCode = HttpStatusCode.BadRequest;
+            }
+            base.OnActionExecuting(actionContext);
+        }
     }
 }
