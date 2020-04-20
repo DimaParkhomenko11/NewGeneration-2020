aceea52da7a4dca4454f95a66f99f6868a37fd2f
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-04-20 22:02:03 +0300

    Provider class added

---
+++ Gallery/Gallery.DAL/Providers/CustomRoleProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+using Gallery.DAL.Models;
+
+namespace Gallery.DAL.Providers
+{
+    public class CustomRoleProvider : RoleProvider
+    {
+        public override string[] GetRolesForUser(string username)
+        {
+            string[] roles = new string[] { };
+            UserContext db = new UserContext();
+
+            // Получаем пользователя
+            User user = db.Users.Include(u => u.Role).FirstOrDefault(u => u.Email == username);
+            if (user != null && user.Role != null)
+            {
+                // получаем роль
+                roles = new string[] { user.Role.Name };
+            }
+            return roles;
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            UserContext db = new UserContext();
+
+            // Получаем пользователя
+            User user = db.Users.Include(u => u.Role).FirstOrDefault(u => u.Email == username);
+
+            if (user != null && user.Role != null && user.Role.Name == roleName)
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
