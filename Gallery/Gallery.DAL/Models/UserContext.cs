5cc9df2f17bc9b30387173e654265b2cb4b102c9
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-04-28 21:43:01 +0300

    Database Connection Name Changed

--- Gallery/Gallery.DAL/Models/UserContext.cs
+++ Gallery/Gallery.DAL/Models/UserContext.cs
@@ -6,7 +6,7 @@ namespace Gallery.DAL.Models
     public class UserContext : DbContext
     {
 
-        public UserContext() : base("DefaultConnection")
+        public UserContext() : base("SQLDB")
         { }
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
