403b493235d278cdae114b6c667dd95b59fffb70
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-04-30 22:01:00 +0300

    UserDbInitializer class changed

--- Gallery/Gallery.DAL/Models/UserContext.cs
+++ Gallery/Gallery.DAL/Models/UserContext.cs
@@ -19,7 +19,7 @@ namespace Gallery.DAL.Models
     {
         protected override void Seed(UserContext db)
         {
-            /*User user1 = new User { Id = 1, Email = "dima@ukr.net", Password = "123"};
+            User user1 = new User { Id = 1, Email = "dima@ukr.net", Password = "123"};
             User user2 = new User { Id = 1, Email = "admin@ukr.net", Password = "123" };
 
             db.Users.AddRange(new List<User> { user1, user2});
@@ -31,19 +31,12 @@ namespace Gallery.DAL.Models
             role1.Users.Add(user2);
             role2.Users.Add(user1);
             role3.Users.Add(user1);
-            db.Roles.Add(role1);
-            db.Roles.Add(role2);
-            db.Roles.Add(role3);
-            db.SaveChanges();*/
-
-            User user = new User {Id = 1, Email = "dima@ukr.net", Password = "123"};
-            
-            db.Users.Add(user);
+            db.Roles.AddRange(new List<Role>{role1, role2, role3});
             db.SaveChanges();
 
-            Media pl1 = new Media {Name = "Media1", User = user };
-            Media pl2 = new Media { Name = "Media2", User = user }; 
-            Media pl3 = new Media { Name = "Media3", User = user };
+            Media pl1 = new Media {Name = "Media1", User = user1 };
+            Media pl2 = new Media { Name = "Media2", User = user2 }; 
+            Media pl3 = new Media { Name = "Media3", User = user2 };
             db.Medias.AddRange(new List<Media> { pl1, pl2, pl3 });
             db.SaveChanges();
 
