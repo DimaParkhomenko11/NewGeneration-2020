88608f278104be8881ad9e3c1f1b830f42137cc8
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-05-08 02:35:09 +0300

    Added new migration

---
+++ Gallery/Gallery.DAL/Migrations/202005072329394_Migration-v3.cs
@@ -0,0 +1,35 @@
+﻿namespace Gallery.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Migrationv3 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Attempts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        TimeStamp = c.DateTime(nullable: false),
+                        Success = c.Boolean(nullable: false),
+                        IpAddress = c.String(),
+                        UserId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            AlterColumn("dbo.Users", "Email", c => c.String(nullable: false, maxLength: 40, unicode: false));
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Attempts", "UserId", "dbo.Users");
+            DropIndex("dbo.Attempts", new[] { "UserId" });
+            AlterColumn("dbo.Users", "Email", c => c.String(nullable: false, maxLength: 25, unicode: false));
+            DropTable("dbo.Attempts");
+        }
+    }
+}
