cbf1d28a3bd8f4d115a4d101362ed5126be9b074
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-05-08 17:05:13 +0300

    Added new migration

---
+++ Gallery/Gallery.DAL/Migrations/202005081257095_Migration-v4.cs
@@ -0,0 +1,18 @@
+﻿namespace Gallery.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Migrationv4 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Attempts", "TimeStamp", c => c.DateTime(nullable: false, precision: 7, storeType: "datetime2"));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Attempts", "TimeStamp", c => c.DateTime(nullable: false));
+        }
+    }
+}
