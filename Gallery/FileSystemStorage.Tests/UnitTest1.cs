8b0f3bed67afd03d8801a25d2f11c3c95417744b
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-05-14 17:02:48 +0300

    Added unit tests for mediaProvider.Upload()

--- Gallery/FileSystemStorage.Tests/UnitTest1.cs
+++ Gallery/FileSystemStorage.Tests/UnitTest1.cs
@@ -1,18 +1,53 @@
+using System;
+using System.IO;
+using Gallery.BLL.Interfaces;
+using Moq;
 using NUnit.Framework;
 
 namespace FileSystemStorage.Tests
 {
+    [TestFixture]
     public class Tests
     {
+
         [SetUp]
         public void Setup()
         {
         }
 
         [Test]
-        public void Test1()
+        public void UploadTest()
         {
-            Assert.Pass();
+            string path =
+                "D:\\GitHub\\NewGeneration\\NewGeneration2020\\Gallery\\FileSystemStorage.Tests\\Files\\FileTest.png";
+            byte[] bytes = new byte[] { };
+
+            MediaProvider mediaProvider = new MediaProvider();
+
+            Assert.AreEqual(true, mediaProvider.Upload(bytes, path));
+        }
+
+        [Test]
+        public void UploadFalsePathTest()
+        {
+            string path =
+                "D:\\GitHub\\NewGeneration\\NewGeneration2020\\Gallery\\FileSystemStorage.Tests\\Files\\";
+            byte[] bytes = new byte[] { };
+
+            MediaProvider mediaProvider = new MediaProvider();
+
+            Assert.Throws<DirectoryNotFoundException>(() => mediaProvider.Upload(bytes, path));
+        }
+
+        [Test]
+        public void UploadNullPathTest()
+        {
+            string path = null;
+            byte[] bytes = new byte[] { };
+
+            MediaProvider mediaProvider = new MediaProvider();
+
+            Assert.Throws<ArgumentNullException>(() => mediaProvider.Upload(bytes, path));
         }
     }
 }
