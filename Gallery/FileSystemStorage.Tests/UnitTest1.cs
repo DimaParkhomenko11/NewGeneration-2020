c4187c857712ce68d81389b37841a7fda25cd76f
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-05-15 01:14:53 +0300

    Changed the name of the tests

--- Gallery/FileSystemStorage.Tests/UnitTest1.cs
+++ Gallery/FileSystemStorage.Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using Gallery.BLL.Interfaces;
+using Gallery.BLL.Services;
 using Moq;
 using NUnit.Framework;
 
@@ -16,19 +17,20 @@ namespace FileSystemStorage.Tests
         }
 
         [Test]
-        public void UploadTest()
+        public void Upload_CorrectValue_ReturnTrue()
         {
             string path =
                 "D:\\GitHub\\NewGeneration\\NewGeneration2020\\Gallery\\FileSystemStorage.Tests\\Files\\FileTest.png";
             byte[] bytes = new byte[] { };
 
             MediaProvider mediaProvider = new MediaProvider();
+            var result = mediaProvider.Upload(bytes, path);
 
-            Assert.AreEqual(true, mediaProvider.Upload(bytes, path));
+            Assert.IsTrue(result);
         }
 
         [Test]
-        public void UploadFalsePathTest()
+        public void Upload_InCorrectPath_ReturnDirectoryNotFoundException()
         {
             string path =
                 "D:\\GitHub\\NewGeneration\\NewGeneration2020\\Gallery\\FileSystemStorage.Tests\\Files\\";
@@ -40,7 +42,7 @@ namespace FileSystemStorage.Tests
         }
 
         [Test]
-        public void UploadNullPathTest()
+        public void Upload_NullPath_ReturnArgumentNullException()
         {
             string path = null;
             byte[] bytes = new byte[] { };
