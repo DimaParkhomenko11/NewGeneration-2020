f0960ffc4d24051b662b3cc16ad8fd2cd217b3c2
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-05-19 22:21:18 +0300

    AttemptDTO class created

---
+++ Gallery/Gallery.BLL/Contract/AttemptDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.BLL.Contract
+{
+    public class AttemptDTO
+    {
+        public string Email { get; set; }
+        public string IpAddress { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+}
