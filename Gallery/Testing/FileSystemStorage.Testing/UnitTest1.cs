58927ca1b601025668354e64f46c39d1ab9199a6
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-05-22 13:46:53 +0300

    FileSystemStorage.Testing moved to Testing folder

---
+++ Gallery/Testing/FileSystemStorage.Testing/UnitTest1.cs
@@ -0,0 +1,53 @@
+using System;
+using System.IO;
+using Moq;
+using NUnit.Framework;
+
+namespace FileSystemStorage.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Upload_CorrectValue_ReturnTrue()
+        {
+            string path =
+                "D:\\GitHub\\NewGeneration\\NewGeneration2020\\Gallery\\FileSystemStorage.Tests\\Files\\FileTest.png";
+            byte[] bytes = new byte[] { };
+
+            MediaProvider mediaProvider = new MediaProvider();
+            var result = mediaProvider.Upload(bytes, path);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Upload_InCorrectPath_ReturnDirectoryNotFoundException()
+        {
+            string path =
+                "D:\\GitHub\\NewGeneration\\NewGeneration2020\\Gallery\\FileSystemStorage.Tests\\Files\\";
+            byte[] bytes = new byte[] { };
+
+            MediaProvider mediaProvider = new MediaProvider();
+
+            Assert.Throws<DirectoryNotFoundException>(() => mediaProvider.Upload(bytes, path));
+        }
+
+        [Test]
+        public void Upload_NullPath_ReturnArgumentNullException()
+        {
+            string path = null;
+            byte[] bytes = new byte[] { };
+
+            MediaProvider mediaProvider = new MediaProvider();
+
+            Assert.Throws<ArgumentNullException>(() => mediaProvider.Upload(bytes, path));
+        }
+    }
+}
