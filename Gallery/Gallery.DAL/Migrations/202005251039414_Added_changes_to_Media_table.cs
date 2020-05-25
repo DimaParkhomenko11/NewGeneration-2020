cba72d4bd37a743307466c062d69546ec9241ddc
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-05-25 13:42:00 +0300

    New migration added

---
+++ Gallery/Gallery.DAL/Migrations/202005251039414_Added_changes_to_Media_table.cs
@@ -0,0 +1,20 @@
+﻿namespace Gallery.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Added_changes_to_Media_table : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Media", "PathToMedia", c => c.String(nullable: false, maxLength: 500));
+            CreateIndex("dbo.Media", "PathToMedia", unique: true);
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.Media", new[] { "PathToMedia" });
+            AlterColumn("dbo.Media", "PathToMedia", c => c.String(maxLength: 8000, unicode: false));
+        }
+    }
+}
