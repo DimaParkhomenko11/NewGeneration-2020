be994ee0ca1e2d74b47dd7289e9ac360e5026569
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-05-25 13:10:52 +0300

    New migration added

---
+++ Gallery/Gallery.DAL/Migrations/202005242204115_Removed_path_field_restriction_in_Media.cs
@@ -0,0 +1,18 @@
+﻿namespace Gallery.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Removed_path_field_restriction_in_Media : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Media", "PathToMedia", c => c.String(maxLength: 8000, unicode: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Media", "PathToMedia", c => c.String(maxLength: 25, unicode: false));
+        }
+    }
+}
