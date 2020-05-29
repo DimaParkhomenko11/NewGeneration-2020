e8d192c964f517e360c66a2e1595cb9be1c7564c
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-05-29 15:56:20 +0300

    Added new migration

---
+++ Gallery/Gallery.DAL/Migrations/202005291254282_Renamed_the_UniqueIdentificator_field_to_UniqueIdentName_in_TemporaryMedia.cs
@@ -0,0 +1,20 @@
+﻿namespace Gallery.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Renamed_the_UniqueIdentificator_field_to_UniqueIdentName_in_TemporaryMedia : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.TemporaryMedia", "UniqueIdentName", c => c.String(nullable: false));
+            DropColumn("dbo.TemporaryMedia", "UniqueIdentificator");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.TemporaryMedia", "UniqueIdentificator", c => c.String(nullable: false));
+            DropColumn("dbo.TemporaryMedia", "UniqueIdentName");
+        }
+    }
+}
