3beb0ffecc7dc7dcce6c4a5f1720db30936d1960
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-05-29 15:47:49 +0300

    Added IPublisher interface and its implementation

---
+++ Gallery/Gallery.DAL/InterfaceImplementation/Publisher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Messaging;
+using Gallery.DAL.Interfaces;
+
+namespace Gallery.DAL.InterfaceImplementation
+{
+    public class Publisher: IPublisher
+    {
+        public void PublishMessage(byte[] fileBytes, string queuePath, string queueName)
+        {
+            using (var queue = MessageQueue.Create(queuePath))
+            {
+                queue.Send(fileBytes, queueName);
+            }
+        }
+    }
+}
