b6afa9ed74cb15ee074e0242ec6465d1e9de0cf6
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-06-04 19:23:45 +0300

    Added new INamingService interface

---
+++ Gallery/Gallery.BLL/Services/NamingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gallery.BLL.Interfaces;
+
+namespace Gallery.BLL.Services
+{
+    public class NamingService : INamingService
+    {
+    }
+}
