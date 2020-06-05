56ba58705ef5987db663b13189f6eb4d036c70f0
DimaParkhomenko11 <dimonparhom@ukr.net>
2020-06-05 15:46:33 +0300

    Added a new interface and its implementation

---
+++ Gallery/Gallery.MQ/InterfaceImplementation/ConsumerMQ.cs
@@ -0,0 +1,14 @@
+﻿using Gallery.MQ.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.MQ.InterfaceImplementation
+{
+    public class ConsumerMQ : IConsumerMQ
+    {
+
+    }
+}
