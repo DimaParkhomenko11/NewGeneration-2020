120083fe086092d89b8231d81423bd531e9533f8
Дмитрий Пархоменко <dimonparhom12262000@gmail.com>
2020-06-14 20:00:47 +0300

    Added new migration

---
+++ Gallery/Gallery.DAL/Migrations/202006121329172_Added_new_UserPathImages_field_in_TemporaryMedia.cs
@@ -0,0 +1,18 @@
+﻿namespace Gallery.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Added_new_UserPathImages_field_in_TemporaryMedia : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.TemporaryMedia", "UserPathImages", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.TemporaryMedia", "UserPathImages");
+        }
+    }
+}
