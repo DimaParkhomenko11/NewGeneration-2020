41ea1d52b5c496ed7fb8c43b165a6281962ed6f8
Дмитрий Пархоменко <dimonparhom12262000@gmail.com>
2020-06-21 22:16:56 +0300

    Added MessageDto

---
+++ Gallery/Gallery.BLL/Contract/MessageDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.BLL.Contract
+{
+    public class MessageDto
+    {
+        public string UniqueName;
+        public string TempPath;
+        public int UserId;
+        public string UserPath;
+    }
+}
