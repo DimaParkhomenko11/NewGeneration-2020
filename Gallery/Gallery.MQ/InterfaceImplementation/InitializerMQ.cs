9dab25124f1492be722748863aa772bfa8ba98ec
Дмитрий Пархоменко <dimonparhom12262000@gmail.com>
2020-06-23 15:48:06 +0300

    Added initializer and parser for data from the config

---
+++ Gallery/Gallery.MQ/InterfaceImplementation/InitializerMQ.cs
@@ -0,0 +1,19 @@
+﻿using System.Messaging;
+using Gallery.MQ.Interfaces;
+
+namespace Gallery.MQ.InterfaceImplementation
+{
+    public class InitializerMQ : IInitializerMQ
+    {
+        public void Initializer(string[] queues)
+        {
+            foreach (var queuePath in queues)
+            {
+                if (!MessageQueue.Exists(queuePath))
+                {
+                    MessageQueue.Create(queuePath);
+                }
+            }
+        }
+    }
+}
