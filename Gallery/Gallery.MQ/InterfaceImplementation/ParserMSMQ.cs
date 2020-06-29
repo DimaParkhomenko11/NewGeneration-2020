a54d59f5a8589203cf696115691675f44076a782
Дмитрий Пархоменко <dimonparhom12262000@gmail.com>
2020-06-30 00:53:24 +0300

    ParserMSMQ added

---
+++ Gallery/Gallery.MQ/InterfaceImplementation/ParserMSMQ.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Configuration;
+using Gallery.MQ.Interfaces;
+
+namespace Gallery.MQ.InterfaceImplementation
+{
+    public class ParserMSMQ : IParserMQ
+    {
+        public string[] ParserMQ()
+        {
+            var queues = ConfigurationManager.AppSettings["MessageQueuingPath"] ?? throw new ArgumentException();
+            return queues.Split(',');
+        }
+    }
+}
