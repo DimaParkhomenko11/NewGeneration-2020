c2913d4fed6fdc29c28eca8469c91b01137b5f2d
Дмитрий Пархоменко <dimonparhom12262000@gmail.com>
2020-07-06 01:07:32 +0300

    Changed the abstract parser class to a regular one and added a new parser method

--- Gallery/Gallery.MQ/Abstraction/ParserMQ.cs
+++ Gallery/Gallery.MQ/Abstraction/ParserMQ.cs
@@ -1,7 +1,25 @@
-﻿namespace Gallery.MQ.Abstraction
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace Gallery.MQ.Abstraction
 {
-    public abstract class ParserMQ
+    public class ParserMQ
     {
-        public abstract string[] ParserMq();
+        public Dictionary<string, string> ParserMq()
+        {
+            var queueUploadImage = ConfigurationManager.AppSettings["queue:upload-image"] ?? throw new ArgumentException();
+            var queueUploadMp3 = ConfigurationManager.AppSettings["queue:upload-mp3"] ?? throw new ArgumentException();
+            var queueUploadMp4 = ConfigurationManager.AppSettings["queue:upload-mp4"] ?? throw new ArgumentException();
+
+            Dictionary<string, string> queueDictionary = new Dictionary<string, string>
+            {
+                { "queue:upload-image", queueUploadImage },
+                { "queue:upload-mp3", queueUploadMp3 },
+                { "queue:upload-mp4", queueUploadMp4 }
+            };
+            return queueDictionary;
+
+        }
     }
 }
