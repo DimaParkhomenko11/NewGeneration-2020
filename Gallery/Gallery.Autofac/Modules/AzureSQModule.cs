42494ce4b7b2b8a68eb6cac9839fdc8fa27bd4d8
Дмитрий Пархоменко <dimonparhom12262000@gmail.com>
2020-07-10 15:28:46 +0300

    Added AzureSQModule

---
+++ Gallery/Gallery.Autofac/Modules/AzureSQModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using Gallery.ASQ.Implementation;
+using Gallery.Configurations.Management;
+using Gallery.MQ.Abstraction;
+
+namespace Gallery.Autofac.Modules
+{
+    public class AzureSQModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var azureConnectionString = ConfigurationManagement.AzureMqConnectionString();
+            builder.Register(ctx => new PublisherASQ(azureConnectionString))
+                .As<PublisherMQ>();
+        }
+    }
+}
